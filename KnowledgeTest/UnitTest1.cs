7c094e76253e7de91e9b7d5912050f0117b1c063
Dariusz Włoch <Dariusz.Wloch@ibm.com>
2020-07-30 11:19:53 +0200

    Project organization: - Added main folder 'Knowledge' - Added project with tests (KnowledgeTest)

---
+++ KnowledgeTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace KnowledgeTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
